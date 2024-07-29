f6d16b755d6c74bca50db53598a1f4d17c551a17
growthnix <support@h2d.dev>
2024-07-29 12:04:28 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import Navigation from './navigation';
+
+
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.root}>
+      <Navigation />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    backgroundColor: '#F9FBFC',
+  },
+});
+
+export default App;
